f3597755a96c3fac084ba35b26d14bb092c5c93e
Sabituly Nurmukhammed <nurmuhammedsabituly222@mail.ru>
2025-02-04 21:32:44 +0500

    Initial commit: Spring Boot project setup

---
+++ src/test/java/com/example/docker_demo/DockerDemoApplication.java
@@ -0,0 +1,15 @@
+package com.example.docker_demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class DockerDemoApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(DockerDemoApplication.class, args);
+	}
+}
+
